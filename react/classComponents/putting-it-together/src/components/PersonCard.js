823f4a981c31f8fd38793ed6285f5d75cdc98b05
Stephen Lee <stephenabcba@gmail.com>
2022-03-07 22:26:15 -0800

    react with synthetic events and state

---
+++ react/classComponents/putting-it-together/src/components/PersonCard.js
@@ -0,0 +1,28 @@
+import react, { Component } from 'react';
+
+class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: props.age,
+        }
+    }
+
+    increaseAge = (e) => {
+        this.setState({ age: this.state.age + 1 })
+    }
+
+    render() {
+        const { firstName, lastName, hairColor } = this.props
+        return (
+            <div>
+                <h1>{lastName}, {firstName}</h1>
+                <p>Age: {this.state.age}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.increaseAge}>Birthday Button for {firstName} {lastName}</button>
+            </div>
+        )
+    }
+}
+
+export default PersonCard;
