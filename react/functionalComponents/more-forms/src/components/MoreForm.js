f075bcdf9d405d3034c10115533cbeb9bffd06ee
Stephen Lee <stephenabcba@gmail.com>
2022-03-10 15:40:51 -0800

    include data binding and implement processForm

--- react/functionalComponents/more-forms/src/components/MoreForm.js
+++ react/functionalComponents/more-forms/src/components/MoreForm.js
@@ -12,7 +12,29 @@ const MoreForm = (props) => {
     const [confPassword, setConfPassword] = useState("")
     const [confPasswordError, setConfPasswordError] = useState("")
     const [passwordMatchError, setPasswordMatchError] = useState("")
-    const processForm = () => "form processed"
+
+    const processForm = (e) => {
+        e.preventDefault()
+        console.log("form processed")
+        const user = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        }
+        console.log("welcome", user)
+        setFirstName("")
+        setFirstNameError("")
+        setLastName("")
+        setLastNameError("")
+        setEmail("")
+        setEmailError("")
+        setPassword("")
+        setPasswordError("")
+        setConfPassword("")
+        setConfPasswordError("")
+        setPasswordMatchError("")
+    }
 
     const handleFirstName = (e) => {
         setFirstName(e.target.value)
@@ -68,10 +90,10 @@ const MoreForm = (props) => {
 
     return (
         <>
-            <form onSubmit={processForm} class="form">
+            <form onSubmit={processForm} className="form">
                 <div className="border p-3 mx-5 bg-light mb-3 rounded d-flex align-items-center" >
                     <label htmlFor="firstName" className="form-label col-1">First Name</label>
-                    <input type="text" name="firstName" id="firstName" onChange={handleFirstName} className="form-control" />
+                    <input type="text" name="firstName" id="firstName" value={firstName} onChange={handleFirstName} className="form-control" />
                 </div>
                 {
                     firstNameError ?
@@ -80,7 +102,7 @@ const MoreForm = (props) => {
                 }
                 <div className="border p-3 mx-5 bg-light mb-3 rounded d-flex align-items-center">
                     <label htmlFor="lastName" className="form-label col-1">Last Name</label>
-                    <input type="text" name="lastName" id="lastName" onChange={handleLastName} className="form-control" />
+                    <input type="text" name="lastName" id="lastName" value={lastName} onChange={handleLastName} className="form-control" />
                 </div>
                 {
                     lastNameError ?
@@ -89,7 +111,7 @@ const MoreForm = (props) => {
                 }
                 <div className="border p-3 mx-5 bg-light mb-3 rounded d-flex align-items-center">
                     <label htmlFor="email" className="form-label col-1">Email</label>
-                    <input type="text" name="email" id="email" onChange={handleEmail} className="form-control" />
+                    <input type="text" name="email" id="email" value={email} onChange={handleEmail} className="form-control" />
                 </div>
                 {
                     emailError ?
@@ -98,7 +120,7 @@ const MoreForm = (props) => {
                 }
                 <div className="border p-3 mx-5 bg-light mb-3 rounded d-flex align-items-center">
                     <label htmlFor="password" className="form-label col-1">Password</label>
-                    <input type="password" name="password" id="password" onChange={handlePassword} className="form-control" />
+                    <input type="password" name="password" id="password" value={password} onChange={handlePassword} className="form-control" />
                 </div>
                 {
                     passwordError ?
@@ -107,7 +129,7 @@ const MoreForm = (props) => {
                 }
                 <div className="border p-3 mx-5 bg-light mb-3 rounded d-flex align-items-center">
                     <label htmlFor="confPassword" className="form-label col-1">Confirm Password</label>
-                    <input type="password" name="confPassword" id="confPassword" onChange={handleConfPassword} className="form-control" />
+                    <input type="password" name="confPassword" id="confPassword" value={confPassword} onChange={handleConfPassword} className="form-control" />
                 </div>
                 {
                     confPasswordError ?
@@ -119,6 +141,7 @@ const MoreForm = (props) => {
                         <p className="mx-5">{passwordMatchError} </p>
                         : ""
                 }
+                <button>Submit</button>
             </form>
         </>
     )
